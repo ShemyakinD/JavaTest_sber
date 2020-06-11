bee0ade44f198de599edd07bf5f325bc2f0997de
Dmitry Shemyakin <dmi5938@mail.ru>
2020-06-11 20:14:07 +0500

    Added task2

---
+++ src/Task2/Main1.java
@@ -0,0 +1,55 @@
+package Task2;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        //Вобъём случайный массив целых чисел
+        int[] testArray = new int[]{4,6,7,213,634,8,56,34,2,3,25,75,4,64,7,7,9,0,-91,-2,421,-2};
+        //Создадим экземпляр нашего рукотворного класса и выведем его
+        UltraArray ultraArray = new UltraArray(testArray);
+        ultraArray.print();
+
+        System.out.println("--------------add");
+        //Добавим новое значение -5 в массив с индексом 2
+        ultraArray.add(-5,2);
+        //Добавим новое значение 99999 в конец массива
+        ultraArray.add(99999);
+        ultraArray.print();//Проверяем
+
+        System.out.println("--------------add out of bound");
+        //Попробуем добавить элемент, а в индекс передадим позицию за пределами массива большую на 1
+        ultraArray.add(5555,ultraArray.getSize());
+        ultraArray.print();//Проверяем, должно встать справа в массиве, если бы передали ещё больше, должен быть OutOfBound
+
+        System.out.println("--------------remove");
+        //Удаляем добавленую ранее -5 с индексом 2
+        ultraArray.remove(2);
+        ultraArray.print();//Проверяем
+
+        System.out.println("--------------set");
+        //Поменяем текущий элемент с индексом 2 на -11111
+        ultraArray.set(2,-11111);
+        //Сразу попробуем поменять элемент, который находится на 1 за пределами массива (т.е. не существующий)
+        ultraArray.set(ultraArray.getSize(),55555);
+        ultraArray.print();//Проверяем, он должен добавиться, аналогично add
+
+        System.out.println("--------------sortDesc");
+        //Сортируем по убыванию и выводим сортирнутую версию и оригинал
+        ultraArray.sortDesc().print();
+        ultraArray.print();
+
+        System.out.println("--------------sortAsc");
+        //Сортируем по возрастанию и выводим сортирнутую версию и оригинал
+        ultraArray.sortAsc().print();
+        ultraArray.print();
+
+        System.out.println("--------------getMax");
+        ultraArray.getMax();//Получим максимум массива
+        System.out.println("--------------getMin");
+        ultraArray.getMin();//Получим минимум массива
+
+        System.out.println("--------------fill same value");
+        ultraArray.refillSameShit(0);//Перезапишем массив одним значением
+        ultraArray.print();
+    }
+}
---
+++ src/Task2/UltraArray.java
@@ -0,0 +1,138 @@
+package Task2;
+
+/**
+ * Класс для работы с массивом целочисленных значений. Задание 2.
+ */
+public class UltraArray {
+    private int[] ultraArray;
+
+    //Конструктор
+    public UltraArray(int[] ultraArray) {
+        this.ultraArray = ultraArray;
+    }
+
+    //Добавление элемента в массив по индексу
+    public void add(int element, int index){
+        int[] newArray = new int[this.ultraArray.length + 1]; //Создадим новый расширенный массив
+        boolean inserted = false; //флаг была ли вставка
+        for (int i = 0; i < newArray.length; i++){
+            if (i == index){ //Если пора добавлять элемент, вставляем и меняем флаг, т.к. была вставка
+                newArray[i] = element;
+                inserted = true;
+            }
+            else if (!inserted) newArray[i] = this.ultraArray[i]; //Если вставки не было идём по порядку
+            else newArray[i] = this.ultraArray[i-1]; //Если была вставка, нужно учитывать шаг в 1 элемент между значениями массивов
+        }
+        this.ultraArray = newArray; //обновлняем наш экземпляр
+    }
+
+    //Добавление элемента в конец массива
+    public void add(int element){
+        this.add(element,this.ultraArray.length);
+    }
+
+    //Вывод количества элементов в массиве
+    public void printSize(){
+        System.out.println(this.ultraArray.length);
+    }
+
+    //Получение количества элементов в массиве
+    public int getSize(){
+        return this.ultraArray.length;
+    }
+
+    //Удаление элемента по индексу
+    public void remove(int index){
+        int[] newArray = new int[this.ultraArray.length - 1]; //Делаем уменьшенный массив для текущих данных
+        boolean deleted = false; //флаг удаления элемента
+        for (int i = 0; i < newArray.length; i++){
+            if (i == index || deleted) { //Если пора удалять элемент или уже удалён, то учитываем шаг между элементами массивов
+                deleted = true;
+                newArray[i] = this.ultraArray[i+1];
+            }
+            else newArray[i] = this.ultraArray[i]; //иначе просто переносим массив
+        }
+        this.ultraArray = newArray; //обновляем экземпляр
+    }
+
+    //Изменение элемента по индексу
+    public void set(int index, int newValue){
+        //Если передаём элемент на 1 больше, чем размер массива, то можно его добавить вместо ошибки
+        if (index == this.ultraArray.length) add(newValue);
+        else {
+            for (int i = 0; i < this.ultraArray.length; i++) {
+                if (i == index) this.ultraArray[i] = newValue;
+            }
+        }
+    }
+
+    //Вывод массива на экран
+    public void print(){
+        for (int element : this.ultraArray) {
+            System.out.printf("%s ",element);
+        }
+        System.out.println("\n");
+    }
+
+    //Сортировка пузырьком по возрастанию
+    public UltraArray sortAsc(){
+        int[] newArray = new int[getSize()]; //Чтобы не изменить исходный массив сделаем его копию
+        for (int i = 0 ; i< getSize();i++)
+            newArray[i] = this.ultraArray[i];
+        for(int i = newArray.length-1 ; i > 0 ; i--){ //переносим в правую часть массива значения по возрастанию
+            for(int j = 0 ; j < i ; j++){
+                if( newArray[j] > newArray[j+1] ){//если текущий элемент больше следующего - двигаем его вправо
+                    int tmp = newArray[j];
+                    newArray[j] = newArray[j+1];
+                    newArray[j+1] = tmp;
+                }
+            }
+        }
+        return new UltraArray(newArray);
+    }
+
+    //Сортировка пузырьком по убыванию
+    public UltraArray sortDesc(){
+        int[] newArray = new int[getSize()]; //Чтобы не изменить исходный массив сделаем его копию
+        for (int i = 0 ; i< getSize();i++)
+            newArray[i] = this.ultraArray[i];
+        for(int i = newArray.length-1 ; i > 0 ; i--){//переносим в правую часть массива значения по убыванию
+            for(int j = 0 ; j < i ; j++){
+                if( newArray[j] < newArray[j+1] ){//если текущий элемент меньше следующего - двигаем его вправо
+                    int tmp = newArray[j];
+                    newArray[j] = newArray[j+1];
+                    newArray[j+1] = tmp;
+                }
+            }
+        }
+        return new UltraArray(newArray);
+    }
+
+    //Вывод максимального значения элемента массива
+    public void getMax(){
+        int max = Integer.MIN_VALUE;
+        for (int i : this.ultraArray){
+            if (max < i) max = i;
+        }
+        System.out.printf("Максимальное значение элемента массива: %s\n",max);
+    }
+
+    //Вывод минимального значения элемента массива
+    public void getMin(){
+        int min = Integer.MAX_VALUE;
+        for (int i : this.ultraArray){
+            if (min > i) min = i;
+        }
+        System.out.printf("Минимальное значение элемента массива: %s\n",min);
+    }
+
+    //Заполнение массива одинаковым значением element
+    public void refillSameShit(int element){
+        int[] newArray = new int[getSize()];
+        for (int i = 0; i < newArray.length; i++){
+            newArray[i] = element;
+        }
+        this.ultraArray = newArray;
+    }
+
+}
